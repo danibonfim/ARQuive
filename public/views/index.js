54b9ab1c5a11d9dbd8a157b83ffad21f9e32de69
Dani <danibonfim@Outlook.com>
2020-06-01 00:16:14 -0300

    começando o guia

--- public/views/index.js
+++ public/views/index.js
@@ -4,7 +4,23 @@ btnReq.addEventListener('click', event => {
     toggle('requirementsPopup')
 });
 
-let closeBtn = document.getElementById('closeBtn');
+let closeBtn = document.getElementById('closeBtnReq');
 closeBtn.addEventListener('click', event => {
     toggle('requirementsPopup')
+});
+
+
+
+//GUIA DO USUARIO
+
+const btnGuide = document.getElementById("btnGuide");
+btnGuide.addEventListener('click', event=>{
+    toggle('guidePopup');
+});
+
+
+
+let closeGuide = document.getElementById('closeBtnGuide');
+closeGuide.addEventListener('click', event =>{
+    toggle('guidePopup');
 });
